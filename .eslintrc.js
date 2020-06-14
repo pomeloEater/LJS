474f42ef49c1a913d3b7a831f11c4d4f6480eea2
Park JinHui <jinhui.dev@gmail.com>
2020-06-14 16:40:02 +0900

    TOOLS SETTING DONE !

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {
+    /* changed comma-dangle default...ironically,
+           we can't use a dangling comma here because
+           this is a JSON file. */
+    "comma-dangle": ["error", "always-multiline"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    semi: ["error", "always"],
+    "no-console": ["off"],
+  },
+};
